125ad200b898305457a40ba3f2863098076be8a0
SantanuAdhikary <santanuadhikary13@gmail.com>
2025-03-17 17:42:32 +0530

    api task using useeffect

---
+++ displayProducts/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+
+    <nav>
+         <h1> MyApp</h1>
+         <input type="text" placeholder='search product' />
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
